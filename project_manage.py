2158eec18116bc62afaf77623d10ec062a3c4422
Yutivich TUNUENG <yutivich2005@gmail.com>
2023-12-17 20:37:52 +0700

    Perform Admin activities

--- project_manage.py
+++ project_manage.py
@@ -20,15 +20,20 @@ ID: {self.__id}"""
 
     def see_database(self):
         for i in self.__database.database:
-            print(i)
+            print(i.table_name)
+            n = 1
+            for j in i.table:
+                print(f"{n}. {j}")
+                n += 1
 
     def create_table(self, table_name):
         self.__database.insert(Table(table_name, []))
 
-    def insert_table(self, table):
-        self.__database.insert(table)
-
-    def insert_row(self, table_name, data):
+    def insert_data(self, table_name, info):
+        data = {}
+        head = get_head(table_name)
+        for i in range(len(head)):
+            data[head[i]] = info[i]
         self.__database.search(table_name).insert(data)
 
     def update_table(self, table_name, row, key, value):
@@ -39,6 +44,84 @@ ID: {self.__id}"""
 
     def remove_row(self, table_name, row):
         self.__database.search(table_name).remove(row)
+
+    def operation(self, choice):
+        if choice == "1":
+            """See Database"""
+            self.see_database()
+        elif choice == "2":
+            """Create Table"""
+            table_name = input("Input name for this table (leave it blank to cancel): ")
+            if table_name.strip() == "":
+                return None
+            else:
+                self.create_table(table_name)
+        elif choice == "3":
+            """Insert Data"""
+            txt1 = "Which table you would like to insert data? " \
+                   "(input only number or leave it blank to cancel): "
+            txt2 = "Input number of table again (leave it blank to cancel): "
+            table_name_list = [i.table_name for i in self.__database.database]
+            table_name = get_value(txt1, txt2, table_name_list)
+            if table_name is None:
+                return None
+            head = get_head(table_name)
+            data = [input(f"{i}: ") for i in head]
+            self.insert_data(table_name, data)
+        elif choice == "4":
+            """Update Table"""
+            txt1_name = "Which table you would like to update? " \
+                        "(input only number or leave it blank to cancel): "
+            txt2_name = "Input number of table again (leave it blank to cancel): "
+            txt1_key = "Which key you would like to update? " \
+                       "(input only number or leave it blank to cancel): "
+            txt2_key = "Input number of key again (leave it blank to cancel): "
+            table_name_list = [i.table_name for i in self.__database.database]
+            table_name = get_value(txt1_name, txt2_name, table_name_list)
+            if table_name is None:
+                return None
+            key_list = get_head(table_name)
+            key = get_value(txt1_key, txt2_key, key_list)
+            if key is None:
+                return None
+            txt1_row = "Which row you want to update? " \
+                       "(input only number or leave it blank to cancel): "
+            txt2_row = "Input number of row again (leave it blank to cancel): "
+            row_list = self.__database.search(table_name).table
+            row_dict = get_value(txt1_row, txt2_row, row_list)
+            if row_dict is None:
+                return None
+            row = self.__database.search(table_name).get_row(lambda x: x[key] == row_dict[key])
+            new_value = input("Input the new value to update: ")
+            self.__database.update(row, key, new_value)
+        elif choice == "5":
+            """Remove Table"""
+            txt1 = "Which table you want to remove? " \
+                   "(input only number or leave it blank to cancel): "
+            txt2 = "Input number of table again (leave it blank to cancel): "
+            table_name_list = [i.table_name for i in self.__database.database]
+            table_name = get_value(txt1, txt2, table_name_list)
+            if table_name is None:
+                return None
+            self.remove_table(table_name)
+        elif choice == "6":
+            """Remove Row"""
+            txt1 = "Which table you want to remove row? " \
+                   "(input only number or leave it blank to cancel): "
+            txt2 = "Input number of table again (leave it blank to cancel): "
+            table_name_list = [i.table_name for i in self.__database.database]
+            table_name = get_value(txt1, txt2, table_name_list)
+            if table_name is None:
+                return None
+            txt1_row = "Which row you want to remove? " \
+                       "(input only number or leave it blank to cancel): "
+            txt2_row = "Input number of row again (leave it blank to cancel): "
+            row_list = self.__database.search(table_name).table
+            row_dict = get_value(txt1_row, txt2_row, row_list)
+            if row_dict is None:
+                return None
+            row = self.__database.search(table_name).get_row(lambda x: x == row_dict)
+            self.remove_row(table_name, row)
 
 
 class Student:
@@ -421,17 +504,55 @@ def exit():
     for i in db.database:
         write_csv(i.table_name, db, get_head(i.table_name))
 
+
 # here are things to do in this function:
    # write out all the tables that have been modified to the corresponding csv files
    # By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
    
    # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
+def get_value(txt1, txt2, list_of_something):
+    for i in range(len(list_of_something)):
+        print(f"{i + 1}. {list_of_something[i]}")
+    user_input = input(txt1)
+    if user_input.strip() == "":
+        return None
+    while user_input not in [str(i + 1) for i in range(len(list_of_something))]:
+        user_input = input(txt2)
+        if user_input.strip() == "":
+            return None
+    return list_of_something[int(user_input)-1]
+
+
+def perform(list_of_login):
+    if list_of_login is None:
+        return None
+    if list_of_login[1] == "admin":
+        user = Admin(get_info_dict(db, list_of_login[0]), db)
+        txt = f"""{user}
+1. See Database
+2. Create Table
+3. Insert Data
+4. Update Table
+5. Remove Table
+6. Remove Row
+What to do? (leave it blank to exit): """
+        choice = input(txt)
+        while choice.strip() != "":
+            while choice not in [str(i + 1) for i in range(7)]:
+                choice = input("Input only number of index: ")
+                if choice.strip() == "":
+                    return None
+            user.operation(choice)
+            print()
+            choice = input(txt)
+        return None
 
 
 # make calls to the initializing and login functions defined above
 
 initializing()
-# val = login()
+val = login()
+perform(val)
 # based on the return value for login, activate the code that performs activities according to the role defined for that person_id
 
 # if val[1] = 'admin':
